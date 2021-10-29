47e7e3317e7b97fc405e94370e9754240badf214
anantadw <anantadesta.public@gmail.com>
2021-10-29 21:50:04 +0700

    Memperbarui database

---
+++ database/migrations/2021_10_29_143629_create_streets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStreetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('streets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('urban_village', 50);
+            $table->string('sub_district', 50);
+            $table->char('postal_code', 5)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('streets');
+    }
+}
